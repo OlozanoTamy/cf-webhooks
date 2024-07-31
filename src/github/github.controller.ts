e7600fff9db91cb52607e2f5b7d3c813a8b8a21b
OlozanoTamy <Olozanodeveloper@gmail.com>
2024-07-31 11:40:46 -0500

    First webhook

---
+++ src/github/github.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Headers, Post } from '@nestjs/common';
+import { GithubService } from './github.service';
+
+@Controller('github')
+export class GithubController {
+  constructor(private readonly githubService: GithubService) {}
+
+@Post('/')
+webhookHandler(
+  @Headers( 'x-github-event' ) githubEvent: any,
+  @Body() body: any,
+){
+  console.log({ githubEvent });
+  return { githubEvent };
+}
+
+}
